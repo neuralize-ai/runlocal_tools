5ba8225e85b4417003f4fb98a19ca8a598db6bb0
Ivan <ivan.chan1@outlook.com>
2025-04-27 14:02:19 +0100

    add workflow

--- src/runlocal_tools/__init__.py
+++ src/runlocal_tools/__init__.py
@@ -1,3 +1,4 @@
 # This file makes src/my_python_package a Python package
-
-__version__ = "0.0.1" 
+# These fields may be used by some tools to find version and name information
+__version__ = "0.0.1"
+__name__ = "runlocal_tools" 
